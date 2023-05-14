eb795ee9e46784db629ae8b2f518943ca2703943
wynterjones <wynter@monetizedesign.com>
2023-05-13 20:31:24 -0400

    ..

--- api/chat.js
+++ api/chat.js
@@ -13,10 +13,10 @@ const chat = async (req, res) => {
         { role: 'system', content: req.body.role },
         { role: 'user', content: req.body.message },
       ],
-      temperature: process.env.OPENAI_TEMPERATURE,
-      max_tokens: process.env.OPENAI_MAX_TOKENS,
-      frequency_penalty: process.env.OPENAI_FREQUENCY_PENALTY,
-      presence_penalty: process.env.OPENAI_PRESENCE_PENALTY,
+      temperature: parseInt(process.env.OPENAI_TEMPERATURE),
+      max_tokens: parseInt(process.env.OPENAI_MAX_TOKENS),
+      frequency_penalty: parseInt(process.env.OPENAI_FREQUENCY_PENALTY),
+      presence_penalty: parseInt(process.env.OPENAI_PRESENCE_PENALTY),
     })
 
     if (completion.data.choices[0].message.content) {
