de69cc03f480c1a8d2a9007496186ba82f5dce2f
wynterjones <wynter@monetizedesign.com>
2023-05-13 20:22:02 -0400

    .

--- api/chat.js
+++ api/chat.js
@@ -7,22 +7,18 @@ const openai = new OpenAIApi(configuration)
 
 const chat = async (req, res) => {
   try {
-    const completion = await openai.createChatCompletion(
-      {
-        model: process.env.OPENAI_MODEL,
-        messages: [
-          { role: 'system', content: req.body.role },
-          { role: 'user', content: req.body.message },
-        ],
-        temperature: 0.6,
-        max_tokens: 512,
-        top_p: 1.0,
-        frequency_penalty: 0.5,
-        presence_penalty: 0.7,
-        stream: true,
-      },
-      { responseType: 'stream' }
-    )
+    const completion = await openai.createChatCompletion({
+      model: process.env.OPENAI_MODEL,
+      messages: [
+        { role: 'system', content: req.body.role },
+        { role: 'user', content: req.body.message },
+      ],
+      temperature: 0.6,
+      max_tokens: 512,
+      top_p: 1.0,
+      frequency_penalty: 0.5,
+      presence_penalty: 0.7,
+    })
 
     if (completion.data.choices[0].message.content) {
       return res.status(200).send({
